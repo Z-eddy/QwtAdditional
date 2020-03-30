4b84913836e44fa41045022109b2561fbcd85d08
Z-eddy <576172400@qq.com>
2020-03-30 16:02:10 +0800

    qwt转移

--- QwtAdditional/MyZoomer.cpp
+++ QwtAdditional/MyZoomer.cpp
@@ -4,6 +4,8 @@ MyZoomer::MyZoomer(QWidget *canvas):QwtPlotZoomer(canvas)
 {
 	//点击时显示坐标
 	this->setTrackerMode(QwtPicker::ActiveOnly);
+	//鼠标左键+Ctrl 方框放大
+	this->setMousePattern(QwtEventPattern::MouseSelect1, Qt::LeftButton, Qt::ControlModifier);
 	//鼠标右键+Ctrl 恢复全图视角
 	this->setMousePattern(QwtEventPattern::MouseSelect2, Qt::RightButton, Qt::ControlModifier);
 	//鼠标右键 逐级恢复视角
--- QwtAdditional/MyZoomer.h
+++ QwtAdditional/MyZoomer.h
@@ -2,6 +2,7 @@
 
 #include "qwtadditional_global.h"
 #include<qwt_plot_zoomer.h>
+//自定义zoomer,解决点击时数值显示黑色
 
 class QWTADDITIONAL_EXPORT MyZoomer:public QwtPlotZoomer
 {
